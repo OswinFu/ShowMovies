0c70a1340ebe7fe6e0bf11e27efb981606ef0de0
OswinFu <f20341@hotmail.com>
2025-03-02 21:30:41 +0800

    arrage page index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { ChipSelect } from "../components/chip"; // 引入 ChipSelect
 import { MovieCard } from "../components/card";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
-import { Alert } from "@mui/material";
 import axios from "axios";
 
 export default function Home() {
@@ -13,47 +12,35 @@ export default function Home() {
   const [movies, setMovies] = useState([]); // 存儲從 API 獲取的電影資料
   const [loading, setLoading] = useState(false); // 加載狀態
   const [error, setError] = useState(null); // 錯誤訊息
-  const [isComplete, setIsComplete] = useState(false); //是否完成
   const [moviesTitle, setMoviesTitle] = useState([]);
 
   // 處理標籤點擊
   const handleClick = ({ updateSelectedTagsId, updateSelectedTagsName }) => {
-    console.log("父組件狀態更新前selectedTagsId:", selectedTagsId);
-    setSelectedTagsId(updateSelectedTagsId); // 設置選中的標籤ID
-    console.log("父組件更新後selectedTagsId", updateSelectedTagsId);
-
-    console.log("父組件更新前selectedTagsName", selectedTagsName);
-    setSelectedTagsName(updateSelectedTagsName);
-    console.log("父組件更新後selectedTagsName", updateSelectedTagsName);
+    setSelectedTagsId(updateSelectedTagsId); // 設置更新的標籤ID
+    setSelectedTagsName(updateSelectedTagsName); // 設置更新的標籤Name
   };
 
   // 發送 API 請求
   const fetchMovies = async () => {
     if (selectedTagsId.length === 0) {
       setError(alert("請選擇一個標籤！"));
-      setLoading(false); // 開始加載
-
       return; // 如果沒有選擇標籤，則不發送請求
     }
+
     setLoading(true); // 開始加載
 
     try {
-      // 假設這是你的 API 路徑
+      // 後端API 路徑，將選擇標籤的ID與Name透過屬性傳給後端API
       const response = await axios.post("/api/movie/byGenres", {
         tagsId: selectedTagsId,
         tagsName: selectedTagsName,
         error: error,
       });
-      // console.log("返回的資料:", response.data);
 
-      setMovies(response.data.data.results);
-      setMoviesTitle(response.data.tagsName); // 設置電影資料
-      console.log("movies", movies);
-      console.log("moviesTitle", moviesTitle);
-
-      setError(null);
+      setMovies(response.data.data.results); // 設置電影資料
+      setMoviesTitle(response.data.tagsName); // 設置電影標題
     } catch (err) {
-      setError("資料加載失敗！"); // 處理錯誤
+      setError("資料加載失敗！"); // 抓到錯誤，給錯誤訊息
     } finally {
       setLoading(false); // 完成加載
     }
@@ -79,7 +66,6 @@ export default function Home() {
         />
 
         <div className="btn">
-          {/* 使用ChipSelect組件 */}
           <Button
             className="button"
             type="submit"
@@ -93,6 +79,7 @@ export default function Home() {
           >
             查詢電影
           </Button>
+
           <Button
             className="button"
             type="reset"
@@ -107,6 +94,7 @@ export default function Home() {
           </Button>
         </div>
       </AppBar>
+
       <Box
         className="card-box"
         sx={{
@@ -133,11 +121,11 @@ export default function Home() {
                 movie={movie}
                 moviesTitle={moviesTitle}
                 className="card"
-              /> // 使用map遍历并传递每部电影数据给Card
+              /> // 將電影資訊用MAP給美的電影卡填入資訊
             ))
           : !loading}
         {error && <p style={{ color: "red" }}>{error}</p>}
-        {/* 顯示錯誤訊息 */}
+        {/* 錯誤顯示 */}
       </Box>
     </>
   );
